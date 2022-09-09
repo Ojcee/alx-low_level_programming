fd8f8938600d08edc226bf8fda8c40b2e5255c00
Ojcee <ohjacee@gmail.com>
2022-09-08 18:20:42 -0700

    optimized

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stuio.h>
-#include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
+ * main - Prints "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
  *  followed by a new line, to the standard error.
- *  Return: Always 0 (Success)
+ *  Return: Always 1.
  */
 int main(void)
 {
