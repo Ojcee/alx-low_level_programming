9109c2af24609896a08c77581b3b897b2b01aedb
Ojcee <ohjacee@gmail.com>
2022-09-09 19:00:59 -0700

    Modified

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,10 @@
  */
 int main(void)
 {
-	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++
-	{
-		putcher(ch);
-	}
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putcher(letter);
 	putchar('\n');
 	return (0);
 }
