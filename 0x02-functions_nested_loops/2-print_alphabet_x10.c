605a9b32f39b3e4e85a84b542f6c95449c901ccc
Ojcee <ohjacee@gmail.com>
2022-09-13 19:54:52 -0700

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
-
 /**
- * print_alphabeth_x10 - Entry point for nested loop
+ * print_alphabeth_x10 - function to print abc 10 times
  * Return: Always 0
  */
 void print_alphabeth_x10(void)
