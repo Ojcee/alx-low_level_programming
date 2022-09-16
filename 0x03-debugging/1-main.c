e2a0caf2e12270431a4d60751d8660672eb80775
Ojcee <ohjacee@gmail.com>
2022-09-15 17:12:00 -0700

    Task three

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -17,5 +18,6 @@ int main(void)
 	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
