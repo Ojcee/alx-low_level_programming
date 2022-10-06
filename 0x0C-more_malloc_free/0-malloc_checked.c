a2ab138dd57fde26d6d72c658b4205d246d1af8b
Ojcee <ohjacee@gmail.com>
2022-10-06 14:09:16 -0700

    Task zero why?

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,36 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-
- *malloc_checked - array for prints a string
-
- *@b: number of memory
-
- *Return: void
-
+ * malloc_checked - Creates an array
+ * @b: array size
+ *
+ * Return: Pointer to array
  */
 
-
-
 void *malloc_checked(unsigned int b)
-
 {
-
-void *p;
-
-
-
-p = malloc(b);
-
-if (p == NULL)
-
-{
-
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
 exit(98);
-
+return (ptr);
 }
-
-
-
-return (p);
-
